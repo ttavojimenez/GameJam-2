996dd73177dbfbfe1197e6c19ccef58d14778a51
Nosmow <165520012+nosmow@users.noreply.github.com>
2024-07-23 16:15:31 -0500

    Basic script AnimalController

---
+++ Assets/Scripts/AnimalController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalController : MonoBehaviour
+{
+    public string typeFood;
+    public int life;
+    public int lifeAttempts;
+    public float timeToEat;
+}
